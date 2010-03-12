ca430ac1bad33b76c893c5a79d99135309b7eef7
Christopher Stawarz <cstawarz@mit.edu>
2010-03-12 15:42:22 -0500

    Started work on new buildbot config

---
+++ buildbot/dicarlolab_config.py
@@ -0,0 +1,13 @@
+# To use this configuration, create a symbolic link to this file named
+# "local_config.py"
+
+project_url = 'http://github.com/dicarlolab-mworks'
+buildbot_hostname = 'dicarlo-mwdev.mit.edu'
+webstatus_port = 8010
+
+from slave_pass import slave_pass
+slave_port = 9989
+
+repository_base_url = 'git://github.com/dicarlolab-mworks' 
+installer_destination_path = '/Library/WebServer/Documents/mw'
+installer_download_base_url = 'http://dicarlo-mwdev.mit.edu/mw'
